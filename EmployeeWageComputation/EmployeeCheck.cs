b30397b3cb75219ea5ba0fd58c50138b90610127
Rak65 <rakeshp6505@gmail.com>
2023-04-27 15:00:08 +0530

    Calculate wages for a Month

--- EmployeeWageComputation/EmployeeCheck.cs
+++ EmployeeWageComputation/EmployeeCheck.cs
@@ -8,29 +8,38 @@ namespace EmployeeWageComputation
 {
     public class EmployeeCheck
     {
+        const int IS_FUL_TIME = 1;
+        const int IS_PART_TIME = 2;
+        const int NO_OF_WORKING_DAYS = 20;        //20 working days per month
+        const int EMP_RATE_PER_HOUR = 20;
         public static void MonthEmpWage() 
         {
-            int WORKING_DAYS = 20;        //20 working days per month
             int dayHour = 0;
-            int monthEmpWage;
-            int WAGE_PER_HOUR = 20;
-            Random random = new Random();
-            int empCheck=random.Next(3);
-            switch(empCheck)
+            int empWage = 0;
+            int totalEmpWage = 0;
+            for(int day = 0; day < NO_OF_WORKING_DAYS; day++)
             {
-                case 0:                //Employee is abscent so dayhour=0
-                    dayHour = 0;
-                    break;
-                case 1:                //Employee is present for Full Time so working hour is 8
-                    dayHour = 8;       
-                    break;
-                case 2:                //Employee is working as Part Time so working hour is 4
-                    dayHour = 4;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(3);
+                switch (empCheck)
+                {
+                    case IS_FUL_TIME:
+                        dayHour = 8;
+                        break;
+                    case IS_PART_TIME :                
+                        dayHour = 4;
+                        break;
+                    default:             
+                        dayHour = 0;
+                        break;
 
+                }
+                empWage = dayHour * EMP_RATE_PER_HOUR;
+                Console.WriteLine("Employee wage = "+empWage);
+                totalEmpWage += empWage;
             }
-            monthEmpWage = dayHour * WAGE_PER_HOUR * WORKING_DAYS;
-            Console .WriteLine("Monthly Employee wage = "+monthEmpWage);
+            Console .WriteLine ("Total Employee wage = "+totalEmpWage );
+
         }
     }
 }

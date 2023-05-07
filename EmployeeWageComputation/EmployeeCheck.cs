a0ffa350c6c9eab0091d22eedabbc6f716d6ea9a
Rak65 <rakeshp6505@gmail.com>
2023-05-07 19:42:07 +0530

    Ability to compute Employee wage for multiple companies

--- EmployeeWageComputation/EmployeeCheck.cs
+++ EmployeeWageComputation/EmployeeCheck.cs
@@ -10,10 +10,7 @@ namespace EmployeeWageComputation
     {
         const int IS_FUL_TIME = 1;
         const int IS_PART_TIME = 2;
-        const int NO_OF_WORKING_DAYS = 20;        //20 working days per month
-        const int EMP_RATE_PER_HOUR = 20;
-        const int MAX_WORKING_HOUR = 100;
-        public static int ComputeEmpWage() 
+        public static int ComputeEmpWage(string company, int EMP_RATE_PER_HOUR, int NO_OF_WORKING_DAYS, int MAX_WORKING_HOUR) 
         {
             int dayHour = 0;
             int totalHours= 0;
--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -10,7 +10,15 @@ namespace EmployeeWageComputation
     {
         static void Main(string[] args)
         {
-            EmployeeCheck.ComputeEmpWage();
+            Console.WriteLine("Enter the Company name : ");
+            String company=Console.ReadLine();
+            Console.WriteLine("Enter Employee rate per hour : ");
+            int EMP_RATE_PER_HOUR=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number of working days :");
+            int NO_OF_WORKING_DAYS=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter maximum working hour in a month :");
+            int MAX_WORKING_HOUR=Convert.ToInt32(Console.ReadLine());
+            EmployeeCheck.ComputeEmpWage(company, EMP_RATE_PER_HOUR, NO_OF_WORKING_DAYS, MAX_WORKING_HOUR);
             Console.ReadKey();
         }
     }
